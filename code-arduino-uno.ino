3c1f2ed99e31601e880e7c048422a3dcd5ef9948
JFernandoHerreraC <fernando_ver908@hotmail.com>
2023-11-05 11:33:19 -0600

    It was refactored so that it can accept the second card reader.

--- code-arduino-uno.ino
+++ code-arduino-uno.ino
@@ -2,12 +2,18 @@
 #include <SPI.h>
 #include <MFRC522.h>
 //#include <Keyboard.h>
-// Pines SPI
+// Pines SPI Derecho
 #define RST_PIN 9
 #define SS_PIN 10
+//Pines SPI Izquierdo
+#define RST_PIN_R 3
+#define SS_PIN_R 4
 
-// Instancia a la clase MFRC522
+// Instancia a la clase MFRC522 para el lado derecho
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+// Instancia a la clase MFRC522 para el lado izquierdo
+MFRC522 mfrc522_R(SS_PIN_R, RST_PIN_R);
 
 // Clave de cifrado actuales
 MFRC522::MIFARE_Key keyA = { keyByte: { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF } };
@@ -21,30 +27,34 @@ void setup() {
   while (!Serial)
     ;                  // Bucle que no permite continuar hasta que no se ha abierto el monitor serie
   SPI.begin();         // Iniciar bus SPI
-  mfrc522.PCD_Init();  // Iniciar lector RFID RC522
+  mfrc522.PCD_Init();  // Iniciar lector RFID RC522 derecho
+  mfrc522_R.PCD_Init();  // Iniciar lector RFID RC522 izquerdo
 }
 
 
 void loop() {
   // Si no hay una tarjeta cerca no sigue el programa
-  if (!mfrc522.PICC_IsNewCardPresent()) {
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522_R.PICC_IsNewCardPresent()) {
     return;
   }
 
   // Si hay una tarjeta cerca, que la eleccione
   // En caso contrario que no continúe
-  if (!mfrc522.PICC_ReadCardSerial()) {
+  if (!mfrc522.PICC_ReadCardSerial() || !mfrc522_R.PICC_ReadCardSerial()) {
     return;
   }
 
   // Mostrar información de la tarjeta por el monitor serie
   //  Serial.print(F("UID de la tarjeta:"));
   mostrarByteArray(mfrc522.uid.uidByte, mfrc522.uid.size);  // Motrar el UID
+  mostrarByteArray(mfrc522_R.uid.uidByte, mfrc522_R.uid.size);  // Motrar el UID
   // Detener el lector
   mfrc522.PICC_HaltA();
+  mfrc522_R.PICC_HaltA();
 }
 
 void mostrarByteArray(byte* buffer, byte bufferSize) {
+  if (buffer == 0 && bufferSize == 0) return;
   String arrayPartesUid;
   for (byte i = 0; i < bufferSize; i++) {
     arrayPartesUid.concat(buffer[i]);
